e38c81e289c05fe1fde885b2f27c73e8b3b65e14
koukiito <dev.koukiito@gmail.com>
2024-09-23 20:22:54 +0900

    add: index_locations_on_tow_truck_id

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,2 +1,3 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
 ALTER TABLE users ADD INDEX index_users_on_username(username);
+ALTER TABLE locations ADD INDEX index_locations_on_tow_truck_id(tow_truck_id);
