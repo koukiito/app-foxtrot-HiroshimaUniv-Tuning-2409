b362da3778c65ae93727489bf70250dffc8e17da
koukiito <dev.koukiito@gmail.com>
2024-09-24 10:18:18 +0900

    add: index on dispathers

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -4,3 +4,4 @@ ALTER TABLE locations ADD INDEX index_locations_on_tow_truck_id(tow_truck_id);
 ALTER TABLE edges ADD node_a_area_id INT;
 UPDATE edges SET node_a_area_id = (SELECT area_id FROM nodes WHERE nodes.id = edges.node_a_id);
 ALTER TABLE edges ADD INDEX index_edges_on_node_a_area_id(node_a_area_id);
+ALTER TABLE dispatchers ADD INDEX index_dispatchers_on_user_id(user_id);
