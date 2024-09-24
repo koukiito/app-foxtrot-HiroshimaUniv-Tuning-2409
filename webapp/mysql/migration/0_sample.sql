e3c23cca44ec599c78e830b121ae0c9182ac7275
koukiito <dev.koukiito@gmail.com>
2024-09-24 09:55:29 +0900

    add: index on edges

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -3,3 +3,4 @@ ALTER TABLE users ADD INDEX index_users_on_username(username);
 ALTER TABLE locations ADD INDEX index_locations_on_tow_truck_id(tow_truck_id);
 ALTER TABLE edges ADD node_a_area_id INT;
 UPDATE edges SET node_a_area_id = (SELECT area_id FROM nodes WHERE nodes.id = edges.node_a_id);
+ALTER TABLE edges ADD INDEX index_edges_on_node_a_area_id(node_a_area_id);
